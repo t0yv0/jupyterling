20f2b19a2d43429ef3b315b2f714251103fb41d2
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2026-03-07 18:21:36 -0500

    Clean up py code

--- evaluator.py
+++ evaluator.py
@@ -1,14 +1,21 @@
-import io, sys, traceback
+import io
+import sys
+import traceback
+
 
 _states = {}
 _sid_ctr = [0]
 
+
 def _alloc(base):
-    sid = _sid_ctr[0]; _sid_ctr[0] += 1
+    sid = _sid_ctr[0]
+    _sid_ctr[0] += 1
     _states[sid] = dict(_states[base]) if base in _states else {'__builtins__': __builtins__}
     return sid
 
+
 _initial = _alloc(-1)
+
 
 def run_cell(prev_sid, code):
     sid = _alloc(prev_sid)
@@ -21,10 +28,11 @@ def run_cell(prev_sid, code):
         if s:
             try:
                 v = eval(compile(s, '<cell>', 'eval'), g)
-                if v is not None: result = repr(v)
+                if v is not None:
+                    result = repr(v)
             except SyntaxError:
                 exec(compile(s, '<cell>', 'exec'), g)
-    except:
+    except Exception:
         err = traceback.format_exc()
     finally:
         sys.stdout = old
